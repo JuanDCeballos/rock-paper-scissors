469ff5d3d284b8490739780b4f3df7d28c2508bf
Juan Ceballos <juandavi789@hotmail.com>
2021-07-10 11:50:14 -0500

    ui for the rock paper scissors game, I lern too much from this project

--- js/main.js
+++ js/main.js
@@ -1,58 +1,97 @@
-function computerPlay() {
-  let options = ['rock', 'paper', 'scissors'];
-  let res = options[Math.floor(Math.random() * options.length)];
-  console.log(`By computer: ${res}`);
-  return res;
-  //   return options[Math.floor(Math.random() * options.length)];
-}
+const pPoints = document.querySelector('#playerPoints');
+const cPoints = document.querySelector('#computerPoints');
+const gameResult = document.querySelector('#game');
+const playAgain = document.querySelector('#reload');
+const result = document.querySelector('#result');
+const byPlayer = document.querySelector('#byPlayer');
+const byComputer = document.querySelector('#byComputer');
+const against = document.querySelector('#against');
 
-function playerPlay() {
-  let option = prompt('Type your option', '');
-  let res = option.toLowerCase();
-  console.log(`By player: ${res}`);
-  return res;
-  // console.log(res);
-  //   return option.toLowerCase();
+function computerPlay() {
+  let options = ['ROCK', 'PAPER', 'SCISSORS'];
+  return options[Math.floor(Math.random() * options.length)];
 }
 
 let playerPoints = 0;
 let computerPoints = 0;
+pPoints.textContent = `Player points : ${0}`;
+cPoints.textContent = `Computer points : ${0}`;
 
 function playRound(playerSelection, computerSelection) {
+  result.textContent = '';
+  let a = playerSelection;
   if (playerSelection === computerSelection) {
-    return "It's a tie";
-  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    gameResult.textContent = 'TIE';
+    byPlayer.textContent = `Player selection ${playerSelection}`;
+    byComputer.textContent = `Computer selection ${computerSelection}`;
+    gameResult.style.cssText = 'background-color: #898686; color: #4F504F';
+  } else if (
+    (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+    (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
+    (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')
+  ) {
     playerPoints++;
-    return `You won! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    playerPoints++;
-    return `You won! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    playerPoints++;
-    return `You won! ${playerSelection} beats ${computerSelection}`;
+    pPoints.textContent = `Player points : ${playerPoints}`;
+    byPlayer.textContent = `Player selection ${playerSelection}`;
+    byComputer.textContent = `Computer selection ${computerSelection}`;
+    gameResult.textContent = 'YOU WON';
+    against.textContent = `${playerSelection} beats ${computerSelection}`;
+    gameResult.style.cssText = 'background-color: #22CE46; color: #115E21;';
   } else {
     computerPoints++;
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
+    cPoints.textContent = `Computer points : ${computerPoints}`;
+    byPlayer.textContent = `Player selection ${playerSelection}`;
+    byComputer.textContent = `Computer selection ${computerSelection}`;
+    gameResult.textContent = 'YOU LOSE';
+    against.textContent = `${computerSelection} beats ${playerSelection}`;
+    gameResult.style.cssText = 'background-color: #CE2C22; color: #5E1111;';
+  }
+
+  if (playerPoints == 5) {
+    disableButtons();
+  }
+
+  if (computerPoints == 5) {
+    disableButtons();
   }
 }
 
-function game() {
-  let res;
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = playerPlay();
+function disableButtons() {
+  buttons.forEach((elem) => {
+    elem.disabled = true;
+  });
+}
+
+function checkWinner() {
+  if (playerPoints === 5) {
+    Swal.fire({
+      icon: 'success',
+      title: 'Good job!',
+      text: 'You beat the machine!',
+    });
+  }
+
+  if (computerPoints === 5) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'A machine beats you!',
+    });
+  }
+}
+
+const buttons = document.querySelectorAll('.cta');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const playerSelection = button.id;
     const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection)) + '\n';
-    console.log(playerPoints);
-    console.log(computerPoints);
-  }
+    playRound(playerSelection, computerSelection);
+    checkWinner();
+  });
+});
 
-  if (playerPoints === computerPoints) {
-    return "it's a tie";
-  } else if (playerPoints > computerPoints) {
-    return 'Player won the game';
-  } else {
-    return 'Computer won the game';
-  }
+function reload() {
+  reload = location.reload();
 }
 
-console.log(game());
+playAgain.addEventListener('click', reload, false);
